b8798018f227bd61456116cfe655dbcbeb693fbf
Matheus Moraes <matheusmoraes@MacBook-de-Matheus.local>
2019-12-20 11:44:30 -0300

    g

--- listamodal.js
+++ listamodal.js
@@ -1,1 +1,4 @@
 $(".glis-popup-link.glis-thickbox.tb-added.qvBinded", "body").html("<i class='fa fa-heart'></i>  Adicionar a uma lista");
+
+
+$(".glis-create-form-type .glis-newlisttype", "body").prepend("<label class='modal-label' >Criar nova Lista</label>")
