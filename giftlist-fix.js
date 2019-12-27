1384db5fa389aa060c462419c9be732489371e3a
Matheus Germano de Moraes <matheusgermano97@hotmail.com>
2019-12-27 16:39:54 -0300

    a

---
+++ giftlist-fix.js
@@ -0,0 +1,11 @@
+let myLink = $(".giftlistinfo-actions .list-action .action-manage a").attr('href');
+myLink =  myLink.toLowerCase();
+
+$(".giftlistinfo-actions .list-action .action-manage a").attr('href', myLink);
+
+
+let myLinkEditor = $(".giftlistinfo-actions .list-action .action-edit a").attr('href');
+myLinkEditor = myLinkEditor.toLowerCase();
+
+$(".giftlistinfo-actions .list-action .action-edit a").attr('href', myLinkEditor);
+
